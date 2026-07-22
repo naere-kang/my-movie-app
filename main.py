8da1db51aac4faa3131fb5d9e6f3aea3633a7f50
naere-kang <naereekang2002@yahoo.com>
2026-07-22 18:59:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 
+{
+  "boxOfficeResult": {
+    "boxofficeType": "일별 박스오피스",
+    "showRange": "20260721~20260721",
+    "dailyBoxOfficeList": [
+      {
+        "rank": "1",
+        "movieNm": "영화 제목",
+        "openDt": "2026-07-15",
+        "audiCnt": "820653",
+        "audiAcc": "3410211",
+        "scrnCnt": "1943"
+      },
+      { "rank": "2", "movieNm": "다음 영화", ... }
+    ]
+  }
+}
